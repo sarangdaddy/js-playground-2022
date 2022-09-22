f138f859b9fb02e3216ca6ab6a1ea63adaa61530
sarangdaddy <109648042+sarangdaddy@users.noreply.github.com>
2022-09-22 17:01:08 +0900

    finished practice3

--- 연습문제3/js.js
+++ 연습문제3/js.js
@@ -1,34 +1,49 @@
-const showPrimeNumber = document.querySelector(".show");
+const inputForm = document.querySelector(".input__form");
+const inputMaxNumber = document.querySelector(".input__max-number");
+const showPrimeNumber = document.querySelector(".show__prime-number");
 
 const numbers = {};
+const numbersAll = [];
+const numbersPrime = [];
+let allPrimeNumber = "";
 
-function addAllNumbers() {
-  const numbersAll = [];
-  for (var i = 1; i <= 10000; i++) {
-    numbersAll.push(i);
+function addAll(event) {
+  event.preventDefault();
+  const inputValue = Number(inputMaxNumber.value);
+  inputMaxNumber.value = "";
+  for (var i = 2; i <= inputValue; i++) {
     numbers.all = numbersAll;
+    numbers.all.push(i);
   }
+  addPrime();
 }
 
-const numbersPrime = [];
-const isPrime = true;
-
-if (number <= 1) {
-  isPrime = false;
-} else if (number === 2) {
-  isPrime = true;
-} else {
+function isPrime(number) {
   for (var i = 2; i < number; i++) {
     if (number % i === 0) {
-      isPrime = false;
-      console.log(isPrime, i);
-      break;
+      return false;
     }
+  }
+  return true;
+}
+
+function addPrime() {
+  for (var i = 0; i < numbersAll.length; i++) {
+    var n = numbersAll[i];
+    if (isPrime(n) === true) {
+      numbersPrime.push(n);
+      numbers.prime = numbersPrime;
+    }
+  }
+  getNumbersPrime();
+}
+
+function getNumbersPrime() {
+  allPrimeNumber += `소수는 총 ${numbersPrime.length} 개 입니다.<br><br>`;
+  for (var i = 0; i < numbersPrime.length; i++) {
+    allPrimeNumber += `${i + 1}번째 소수는 ${numbersPrime[i]}입니다.<br>`;
+    showPrimeNumber.innerHTML = allPrimeNumber;
   }
 }
 
-if (isPrime === true) {
-  numbersPrime.push(isPrime);
-}
-
-addAllNumbers();
+inputForm.addEventListener("submit", addAll);
