216134f8c95d9aa3e6a0779de127b45ed110419f
sarang_daddy <sarang_daddy@naver.com>
2022-09-26 23:24:00 +0900

    created gugudan for myself

--- gugudanFinal/js.js
+++ gugudanFinal/js.js
@@ -2,14 +2,29 @@ const inputForm = document.querySelector(".input__form");
 const inputValue = document.querySelector(".input__value");
 const inputShow = document.querySelector(".input__clone");
 
-function getGugudan() {
+function getGugudan(event) {
+  event.preventDefault();
   const results = [];
-  for (var n = 2; n <= 9; n++) {
-    for (var i = 1; i <= 9; i++) {
-      results[i] = n * i;
-    }
+  const inputNumber = inputValue.value;
+  const data = inputNumber.split(",");
+  var n = data[0];
+  var maxNumber = data[1];
+
+  inputValue.value = "";
+  for (var i = 1; i <= maxNumber; i++) {
+    results[i] = n * i;
   }
-  console.log(results);
+  printGugudan(n, maxNumber, results);
+}
+
+function printGugudan(n, maxNumber, results) {
+  for (var i = 1; i <= maxNumber; i++) {
+    let span = document.createElement("span");
+    const br = document.createElement("br");
+    span = `${n} X ${i} = ${results[i]}`;
+    inputShow.append(span);
+    inputShow.append(br);
+  }
 }
 
 inputForm.addEventListener("submit", getGugudan);
