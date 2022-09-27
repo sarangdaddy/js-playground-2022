5e5fb86ca17e009fa7cdeb724cc834c70e86e45c
sarangdaddy <109648042+sarangdaddy@users.noreply.github.com>
2022-09-27 13:52:43 +0900

    i have question

--- gugudanFinal/js.js
+++ gugudanFinal/js.js
@@ -4,12 +4,12 @@ const inputShow = document.querySelector(".input__clone");
 
 function getGugudan(event) {
   event.preventDefault();
+
   const results = [];
   const inputNumber = inputValue.value;
   const data = inputNumber.split(",");
-  var n = data[0];
-  var maxNumber = data[1];
-
+  const n = data[0];
+  const maxNumber = data[1];
   inputValue.value = "";
   for (var i = 1; i <= maxNumber; i++) {
     results[i] = n * i;
@@ -27,4 +27,19 @@ function printGugudan(n, maxNumber, results) {
   }
 }
 
+//** 제거 함수 **//
+function remove() {
+  span = inputShow.querySelectorAll("span");
+  br = inputShow.querySelectorAll("br");
+  inputShow.remove(span);
+  inputShow.remove(br);
+}
+
 inputForm.addEventListener("submit", getGugudan);
+
+// 음
+// n maxNumber 를 입력하였을떄
+// const checkExistItem = ({n : findN,maxNumber : findI}) => {
+//   const result = array.find(({n,maxNumber}) => if(n === findN && maxNumber === findI))
+// }
+// 이런식으로 기존배열에서 입력받은 n i 가있는지 확인해보시면될거에여
