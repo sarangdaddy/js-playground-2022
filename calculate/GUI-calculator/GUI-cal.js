85b814051bdd60e6e79499f73821c3a261a80170
sarang_daddy <sarang_daddy@naver.com>
2022-10-05 23:56:12 +0900

    Update GUI calculator #2

--- calculate/GUI-calculator/GUI-cal.js
+++ calculate/GUI-calculator/GUI-cal.js
@@ -1,6 +1,35 @@
-const input = {};
+const input = { inputArray: [] };
 
-input.inputValues = function (event) {
-  let n = event.target.innerText;
-  this.push(event.target.innerText);
+input.getInput = function () {
+  return this.inputArray.join("");
+};
+
+const clickNumbers = function (event) {
+  let str = event.target.innerText;
+
+  switch (str) {
+    case "Del":
+      input.inputArray.pop();
+      break;
+    case "+":
+      input.inputArray.push(" + ");
+      break;
+    case "-":
+      input.inputArray.push(" - ");
+      break;
+    case "*":
+      input.inputArray.push(" * ");
+      break;
+    case "/":
+      input.inputArray.push(" / ");
+      break;
+    default:
+      input.inputArray.push(str);
+  }
+
+  if (input.inputArray.length === 0) {
+    document.querySelector(".result__clone").innerText = "0";
+  } else {
+    document.querySelector(".result__clone").innerText = input.getInput();
+  }
 };
