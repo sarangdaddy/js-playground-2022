28126332f0b848d82a1dfb9269ff9e08e21e9050
sarangdaddy <109648042+sarangdaddy@users.noreply.github.com>
2022-10-06 17:15:42 +0900

    created GUI-calculator

--- calculate/GUI-calculator/GUI-cal.js
+++ calculate/GUI-calculator/GUI-cal.js
@@ -1,9 +1,6 @@
 const input = { inputArray: [] };
 
-input.getInput = function () {
-  return this.inputArray.join("");
-};
-
+// 클릭시 해당 값 input 하기
 const clickNumbers = function (event) {
   let str = event.target.innerText;
 
@@ -12,24 +9,83 @@ const clickNumbers = function (event) {
       input.inputArray.pop();
       break;
     case "+":
-      input.inputArray.push(" + ");
-      break;
     case "-":
-      input.inputArray.push(" - ");
-      break;
     case "*":
-      input.inputArray.push(" * ");
-      break;
     case "/":
-      input.inputArray.push(" / ");
+      input.inputArray.push(` ${str} `);
       break;
     default:
       input.inputArray.push(str);
   }
 
+  let joinInputArray = input.inputArray.join("");
   if (input.inputArray.length === 0) {
     document.querySelector(".result__clone").innerText = "0";
   } else {
-    document.querySelector(".result__clone").innerText = input.getInput();
+    document.querySelector(".result__clone").innerText = joinInputArray;
   }
+  return joinInputArray;
+};
+
+// join input값 배열로 변경하기
+
+input.getNumber = function (numbersArray) {
+  const n = Number(numbersArray.shift());
+  return n;
+};
+
+input.getOp = function (numbersArray) {
+  const op = numbersArray.shift();
+  return op;
+};
+
+input.removeAll = function (value) {
+  this.inputArray = [];
+  this.inputArray.push(value);
+};
+
+const output = {};
+
+output.print = function (resultValue) {
+  document.querySelector(".result__clone").innerText = `${resultValue}입니다.`;
+};
+
+const calculator = {};
+
+calculator.calculate = function (n1, n2, op) {
+  let calResult = "";
+  switch (op) {
+    case "+":
+      calResult = n1 + n2;
+      break;
+    case "-":
+      calResult = n1 - n2;
+      break;
+    case "*":
+      calResult = n1 * n2;
+      break;
+    case "/":
+      calResult = n1 / n2;
+      break;
+    default:
+      return NaN;
+  }
+  return calResult;
+};
+
+const clickResult = function (event) {
+  let numbersArray = input.inputArray.join("").split(" ");
+  let resultValue = input.getNumber(numbersArray);
+
+  while (true) {
+    let op = input.getOp(numbersArray);
+    if (numbersArray.length === 0) {
+      break;
+    }
+    let n2 = input.getNumber(numbersArray);
+    resultValue = calculator.calculate(resultValue, n2, op);
+  }
+
+  output.print(resultValue);
+  input.removeAll(resultValue);
 };
