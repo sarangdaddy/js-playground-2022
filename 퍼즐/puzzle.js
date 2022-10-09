3c49d22fbcb0098d2f601b8cad5e007b69a6b2a4
sarang_daddy <sarang_daddy@naver.com>
2022-10-09 22:01:37 +0900

    complete the puzzle#2 - create left, right, swap button - checking program

--- 퍼즐/puzzle.js
+++ 퍼즐/puzzle.js
@@ -1,27 +1,57 @@
 const titleStr = document.querySelector(".string");
 const buttonStr = document.querySelector(".string__button");
 
-function getString() {
-  const targetStr = titleStr.innerText;
-  const strArray = targetStr.split("");
-  showString(strArray);
-}
+let targetStr = titleStr.innerText;
+let targetArray = targetStr.split("");
 
-function showString(strArray) {
-  for (let i = 0; i <= strArray.length - 1; i++) {
-    let targetClone = strArray[i];
-    const showTarget = document.createElement("button");
-    showTarget.innerText = targetClone;
-    buttonStr.appendChild(showTarget);
-  }
+for (let i = 0; i < targetStr.length; i++) {
+  let showTarget = document.createElement("button");
+  showTarget.innerHTML = targetArray[i];
+  buttonStr.appendChild(showTarget);
 }
 
 const swap = function (event) {
   console.log("swap");
+  targetArray = targetArray.reverse();
+  console.log(targetArray);
+  targetStr = targetArray.join("");
+  changeStr(targetStr);
 };
 
-const shift = function (event) {
-  console.log("shift");
+const shiftRight = function (event) {
+  console.log("shift__right");
+  let lastArray = targetArray[targetArray.length - 1];
+  targetArray.splice(targetArray.length - 1, 1);
+  targetArray.unshift(lastArray);
+  console.log(targetArray);
+  targetStr = targetArray.join("");
+  changeStr(targetStr);
 };
 
-getString();
+const shiftLeft = function (event) {
+  console.log("shift__left");
+  let lastArray = targetArray[0];
+  targetArray.splice(0, 1);
+  targetArray.push(lastArray);
+  console.log(targetArray);
+  targetStr = targetArray.join("");
+  changeStr(targetStr);
+};
+
+const changeStr = function (targetStr) {
+  for (let i = 0; i < targetStr.length; i++) {
+    let chStr = document.querySelectorAll(".string__button button");
+    chStr[i].innerHTML = targetStr[i];
+    checkStr();
+  }
+};
+
+const checkStr = function () {
+  if (titleStr.innerText === targetArray.join("")) {
+    document.querySelector(".check").innerText = "일치합니다.";
+  } else {
+    document.querySelector(".check").innerText = "일치하지 않습니다.";
+  }
+};
+
+checkStr();
