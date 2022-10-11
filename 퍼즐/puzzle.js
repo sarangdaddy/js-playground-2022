d205df337a75c0484096ff24ed8fa9f504cbd5b5
sarangdaddy <109648042+sarangdaddy@users.noreply.github.com>
2022-10-11 17:24:32 +0900

    complete Puzzle#3
    - create reset game.
    - create progress game.

--- 퍼즐/puzzle.js
+++ 퍼즐/puzzle.js
@@ -1,70 +1,82 @@
-const titleStr = document.querySelector(".string");
+const targetWord = document.querySelector(".string");
 const buttonStr = document.querySelector(".string__button");
 const progress = document.querySelector(".progress");
 
-let game = { strArray: [], btns: [], maxPlay: 3, current: 1 };
+let game = { wordsArray: [], btns: [], maxPlay: 3, current: 0 };
 
 // 여러 단어 중 랜덤으로 시작 단어를 선택
-game.choiceStr = function () {
-  this.strArray = ["HELLO", "SUNGUN", "SOOJIN", "SARANG", "SORRY", "MONEY"];
-  this.randomStrArray =
-    this.strArray[Math.floor(Math.random() * this.strArray.length)];
-  this.targetArray = this.randomStrArray.split("");
-  titleStr.innerHTML = this.targetArray.join("");
-  game.showSrt();
+game.choiceWord = function () {
+  this.wordsArray = ["HELLO", "SUNGUN", "SOOJIN", "SARANG", "SORRY", "MONEY"];
+  this.randomWordStr =
+    this.wordsArray[Math.floor(Math.random() * this.wordsArray.length)];
+  this.randomWordArray = this.randomWordStr.split("");
+  targetWord.innerHTML = this.randomWordArray.join("");
+  game.getButtons();
 };
 
-// 선택돤 단어(targetArray)를 화면에 표시
-game.showSrt = function () {
-  for (let i = 0; i < this.randomStrArray.length; i++) {
+// randomWordSrt의 길이 만큼 button 추가
+game.getButtons = function () {
+  for (let i = 0; i < this.randomWordStr.length; i++) {
     let showTarget = document.createElement("button");
     buttonStr.appendChild(showTarget);
     this.btns.push(showTarget);
   }
 };
 
+// 각 버튼에 텍스트 추가
 game.copyBtnText = function () {
-  for (let i = 0; i < this.randomStrArray.length; i++) {
-    this.btns[i].innerHTML = this.targetArray[i];
+  for (let i = 0; i < this.randomWordStr.length; i++) {
+    this.btns[i].innerHTML = this.randomWordArray[i];
   }
 };
 
 //게임 버튼 (뒤집기, 밀기, 당기기)
+
+game.swap = function () {
+  this.randomWordArray = this.randomWordArray.reverse();
+  this.changeStr();
+};
+
 const swap = function () {
-  game.targetArray = game.targetArray.reverse();
-  targetStr = game.targetArray.join("");
-  changeStr(targetStr);
+  game.swap();
+};
+
+game.shiftRight = function () {
+  let lastArray = this.randomWordArray[this.randomWordArray.length - 1];
+  this.randomWordArray.splice(this.randomWordArray.length - 1, 1);
+  this.randomWordArray.unshift(lastArray);
+  this.changeStr();
 };
 
 const shiftRight = function () {
-  let lastArray = game.targetArray[game.targetArray.length - 1];
-  game.targetArray.splice(game.targetArray.length - 1, 1);
-  game.targetArray.unshift(lastArray);
-  targetStr = game.targetArray.join("");
-  changeStr(targetStr);
+  game.shiftRight();
+};
+
+game.shiftLeft = function () {
+  let lastArray = this.randomWordArray[0];
+  this.randomWordArray.splice(0, 1);
+  this.randomWordArray.push(lastArray);
+  this.changeStr();
 };
 
 const shiftLeft = function () {
-  let lastArray = game.targetArray[0];
-  game.targetArray.splice(0, 1);
-  game.targetArray.push(lastArray);
-  targetStr = game.targetArray.join("");
-  changeStr(targetStr);
+  game.shiftLeft();
 };
 
 // 게임 버튼 실행시 단어 순서 변경
-const changeStr = function (targetStr) {
-  for (let i = 0; i < targetStr.length; i++) {
+game.changeStr = function () {
+  for (let i = 0; i < this.randomWordArray.length; i++) {
     let chStr = document.querySelectorAll(".string__button button");
-    chStr[i].innerHTML = targetStr[i];
-    game.checkStr();
+    chStr[i].innerHTML = this.randomWordArray[i];
+    this.checkStr();
   }
 };
 
 // 변경된 단어 순서와 처음 선택된 단어 매칭 프로그램
 game.checkStr = function () {
-  if (titleStr.innerText === this.targetArray.join("")) {
+  if (targetWord.innerText === this.randomWordArray.join("")) {
     document.querySelector(".check").innerText = "일치합니다.";
+    this.progress();
   } else {
     document.querySelector(".check").innerText = "일치하지 않습니다.";
   }
@@ -75,19 +87,46 @@ game.shuffle = function () {
   const toggle = Math.floor(Math.random() * 2) === 0;
 
   if (toggle) {
-    swap();
+    this.swap();
   }
 
-  const n = Math.floor(Math.random() * (this.randomStrArray.length - 1));
+  const n = Math.floor(Math.random() * (this.randomWordStr.length - 1));
 
-  for (let i = 0; i < n; i++) {
-    shiftRight();
+  for (let i = 0; i <= n; i++) {
+    this.shiftRight();
+  }
+};
+
+//게임을 성공하면 다시 실행
+game.resetGame = function () {
+  let btnsText = document.querySelectorAll(".string__button button");
+  for (let i = 0; i < btnsText.length; i++) {
+    buttonStr.removeChild(btnsText[i]);
+  }
+  this.init();
+};
+
+//게임 성공시 "O" 표시 , 3번 성공시 클리어 멘트
+game.progress = function () {
+  this.current++;
+  this.resetGame();
+  let str = "";
+  for (let i = 0; i < this.current; i++) {
+    str += "O";
+    progress.innerHTML = str;
+  }
+
+  if (this.current === this.maxPlay) {
+    alert("Good! Thanks for playing");
+    str = "";
+    progress.innerHTML = str;
+    this.current = 0;
   }
 };
 
 // 화면 시작과 동시 실행 함수
 game.init = function () {
-  this.choiceStr();
+  this.choiceWord();
   this.copyBtnText();
   this.shuffle();
   this.checkStr();
